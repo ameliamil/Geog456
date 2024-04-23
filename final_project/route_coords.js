31b57a403baae58d462495a7cd6e5c36e82ac66a
ameliamil <99029890+ameliamil@users.noreply.github.com>
2024-04-23 12:59:01 -0400

    Final edits

---
+++ final_project/route_coords.js
@@ -0,0 +1,243 @@
+var route_coords = [
+    {
+        "": "1",
+        "lat": "36.1491032",
+        "lon": "-78.9660358",
+        "mbbs_county": "orange",
+        "route_num": "8"
+    },
+    {
+        "": "2",
+        "lat": "36.23185",
+        "lon": "-79.20792",
+        "mbbs_county": "orange",
+        "route_num": "1"
+    },
+    {
+        "": "3",
+        "lat": "36.1625476",
+        "lon": "-79.0965414",
+        "mbbs_county": "orange",
+        "route_num": "2"
+    },
+    {
+        "": "4",
+        "lat": "36.11949",
+        "lon": "-79.17658",
+        "mbbs_county": "orange",
+        "route_num": "3"
+    },
+    {
+        "": "5",
+        "lat": "35.99296",
+        "lon": "-79.14982",
+        "mbbs_county": "orange",
+        "route_num": "5"
+    },
+    {
+        "": "6",
+        "lat": "35.99957",
+        "lon": "-79.01794",
+        "mbbs_county": "orange",
+        "route_num": "6"
+    },
+    {
+        "": "7",
+        "lat": "36.13213",
+        "lon": "-79.22397",
+        "mbbs_county": "orange",
+        "route_num": "7"
+    },
+    {
+        "": "8",
+        "lat": "35.99968",
+        "lon": "-79.12003",
+        "mbbs_county": "orange",
+        "route_num": "9"
+    },
+    {
+        "": "9",
+        "lat": "35.99973",
+        "lon": "-79.10725",
+        "mbbs_county": "orange",
+        "route_num": "10"
+    },
+    {
+        "": "10",
+        "lat": "35.86652",
+        "lon": "-79.11248",
+        "mbbs_county": "orange",
+        "route_num": "11"
+    },
+    {
+        "": "11",
+        "lat": "35.86703",
+        "lon": "-79.029",
+        "mbbs_county": "orange",
+        "route_num": "12"
+    },
+    {
+        "": "12",
+        "lat": "36.08286",
+        "lon": "-79.09942",
+        "mbbs_county": "orange",
+        "route_num": "4"
+    },
+    {
+        "": "13",
+        "lat": "36.14025",
+        "lon": "-78.95265",
+        "mbbs_county": "durham",
+        "route_num": "1"
+    },
+    {
+        "": "14",
+        "lat": "36.0172651",
+        "lon": "-78.9190844",
+        "mbbs_county": "durham",
+        "route_num": "3"
+    },
+    {
+        "": "15",
+        "lat": "36.09362",
+        "lon": "-78.91018",
+        "mbbs_county": "durham",
+        "route_num": "4"
+    },
+    {
+        "": "16",
+        "lat": "35.88307",
+        "lon": "-78.99265",
+        "mbbs_county": "durham",
+        "route_num": "5"
+    },
+    {
+        "": "17",
+        "lat": "35.87038",
+        "lon": "-78.83388",
+        "mbbs_county": "durham",
+        "route_num": "6"
+    },
+    {
+        "": "18",
+        "lat": "35.95138",
+        "lon": "-78.8356",
+        "mbbs_county": "durham",
+        "route_num": "7"
+    },
+    {
+        "": "19",
+        "lat": "35.92628",
+        "lon": "-78.76183",
+        "mbbs_county": "durham",
+        "route_num": "8"
+    },
+    {
+        "": "20",
+        "lat": "36.12167",
+        "lon": "-78.83963",
+        "mbbs_county": "durham",
+        "route_num": "2"
+    },
+    {
+        "": "21",
+        "lat": "35.76855",
+        "lon": "-79.18405",
+        "mbbs_county": "chatham",
+        "route_num": "1"
+    },
+    {
+        "": "22",
+        "lat": "35.86032",
+        "lon": "-78.9906",
+        "mbbs_county": "chatham",
+        "route_num": "2"
+    },
+    {
+        "": "23",
+        "lat": "35.67302",
+        "lon": "-78.98573",
+        "mbbs_county": "chatham",
+        "route_num": "3"
+    },
+    {
+        "": "24",
+        "lat": "35.5797",
+        "lon": "-79.27682",
+        "mbbs_county": "chatham",
+        "route_num": "4"
+    },
+    {
+        "": "25",
+        "lat": "35.66617",
+        "lon": "-79.48569",
+        "mbbs_county": "chatham",
+        "route_num": "5"
+    },
+    {
+        "": "26",
+        "lat": "35.8186701",
+        "lon": "-79.4742528",
+        "mbbs_county": "chatham",
+        "route_num": "6"
+    },
+    {
+        "": "27",
+        "lat": "35.712907",
+        "lon": "-79.371135",
+        "mbbs_county": "chatham",
+        "route_num": "7"
+    },
+    {
+        "": "28",
+        "lat": "35.784",
+        "lon": "-79.20963",
+        "mbbs_county": "chatham",
+        "route_num": "8"
+    },
+    {
+        "": "29",
+        "lat": "35.7860177",
+        "lon": "-79.0926468",
+        "mbbs_county": "chatham",
+        "route_num": "9"
+    },
+    {
+        "": "30",
+        "lat": "35.6193",
+        "lon": "-79.5208",
+        "mbbs_county": "chatham",
+        "route_num": "12"
+    },
+    {
+        "": "31",
+        "lat": "35.71054",
+        "lon": "-79.54496",
+        "mbbs_county": "chatham",
+        "route_num": "13"
+    },
+    {
+        "": "32",
+        "lat": "35.7471",
+        "lon": "-79.0063",
+        "mbbs_county": "chatham",
+        "route_num": "14"
+    },
+    {
+        "": "33",
+        "lat": "35.6658",
+        "lon": "-79.0966",
+        "mbbs_county": "chatham",
+        "route_num": "10"
+    },
+    {
+        "": "34",
+        "lat": "35.61005",
+        "lon": "-79.27731",
+        "mbbs_county": "chatham",
+        "route_num": "11"
+    },
+    {
+        "": ""
+    }
+]
